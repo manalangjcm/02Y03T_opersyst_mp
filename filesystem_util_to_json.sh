5f8477531537d976ec1a8407fac4cd2f87da9c4e
manalangjcm <159515420+manalangjcm@users.noreply.github.com>
2024-02-10 15:49:09 +0800

    Update filesystem_util_to_json.sh

--- filesystem_util_to_json.sh
+++ filesystem_util_to_json.sh
@@ -37,10 +37,35 @@ function InsertToFile()
 
 function GenerateFileSystemUtilization()
 {	
-	filesystem_utilization=`$filesystem_util_command | awk 'NR>1 {printf "{\"filesystem_no\":\"%d\",\"filesystem\":\"%s\",\"disk_size\":\"%s\",\"disk_used\":\"%s\",\"disk_available\":\"%s\",\"disk_used_pct\":\"%s\",\"directory\":\"%s\"}\n", NR-1, $1, $2, $3, $4, $5, $6}' | jq -s '{ "filesystem_utilization": . }'`
+	filesystem_utilization=$(df -k | awk 'NR>1')
 	
+	filesystem_array=()
+	
+	filesystem_no=1
+	json_format=""
+	
+	while IFS= read -r line; do
+		current_filesystem=$(echo "$line" | awk '{print $1}' | tr -dc '[:print:]\n' | sed -e 's/\\x[[:xdigit:]][[:xdigit:]]//g')
+		current_disk_size=$(echo "$line" | awk '{print $2}')
+		current_disk_used=$(echo "$line" | awk '{print $3}')
+		current_disk_available=$(echo "$line" | awk '{print $4}')
+		current_disk_used_pct=$(echo "$line" | awk '{print $5}' | tr -dc '[:print:]\n' | sed -e 's/\\x[[:xdigit:]][[:xdigit:]]//g')
+		current_directory=$(echo "$line" | awk '{print $6}' | tr -dc '[:print:]\n' | sed -e 's/\\x[[:xdigit:]][[:xdigit:]]//g')
+		
+		json_object="{\"filesystem_no\":\"$filesystem_no\",\"filesystem\":\"$current_filesystem\",\"disk_size\":\"$current_disk_size\",\"disk_used\":\"$current_disk_used\",\"disk_available\":\"$current_disk_size\",\"disk_used_pct\":\"$current_disk_used_pct\",\"directory\":\"$current_directory\"}"
+		final_json_object="{\"filesystem_details\":$json_object},"
+		
+		filesystem_array+=("$final_json_object")
+		
+		((filesystem_no++))
+	done < <(echo "$filesystem_utilization")
+	
+	final_filesystem_array=$(echo "${filesystem_array[@]}" | sed 's/,\([^,]*\)$/\1/')
+	
+	combined_json=$(echo "{\"filesystem_utilization\":[${final_filesystem_array[@]}]}" | jq '.')
+
 	echo "Converting filesystem utilization to JSON..."
-	ConvertToJSON "$filesystem_utilization"
+	ConvertToJSON "$combined_json"
 }
 
 function ConvertToJSON()
@@ -57,10 +82,5 @@ function Main()
 	GenerateFileSystemUtilization
 }
 
-#==========================================================#
-# RUNTIME FUNCTIONS
-#==========================================================#
-
-Main
-
-#==========================================================#
+file_name_log="filesystem_util_to_json_${current_date}.log"
+Main 2>&1 | tee -a "$file_name_log"
