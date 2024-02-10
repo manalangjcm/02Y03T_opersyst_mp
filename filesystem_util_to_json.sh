4c568839e31944ec5c341292df2888076101200a
manalangjcm <159515420+manalangjcm@users.noreply.github.com>
2024-02-10 16:21:21 +0800

    Update filesystem_util_to_json.sh

--- filesystem_util_to_json.sh
+++ filesystem_util_to_json.sh
@@ -27,6 +27,25 @@ filesystem_util_command="df -k"
 # SCRIPT CONTENT
 #==========================================================#
 
+function PrintMessage()
+{
+	local message=$1
+	local log_mode=$2
+	
+	local log_level
+	
+	case $log_mode in
+		0)
+			log_level="INFO"
+			;;
+		1)
+			log_level="ERROR"
+			;;
+	esac
+	
+	echo -e "[`date '+%Y-%m-%d %H:%M:%S'`] [$log_level] : $message"
+}
+
 function InsertToFile()
 {
 	local value_to_insert=$1
@@ -39,11 +58,13 @@ function GenerateFileSystemUtilization()
 {	
 	filesystem_utilization=$(df -k | awk 'NR>1')
 	
+	PrintMessage "Converting filesystem utilization to JSON..." 0
+	
 	filesystem_array=()
 	
 	filesystem_no=1
 	json_format=""
-	
+
 	while IFS= read -r line; do
 		current_filesystem=$(echo "$line" | awk '{print $1}' | tr -dc '[:print:]\n' | sed -e 's/\\x[[:xdigit:]][[:xdigit:]]//g')
 		current_disk_size=$(echo "$line" | awk '{print $2}')
@@ -57,14 +78,15 @@ function GenerateFileSystemUtilization()
 		
 		filesystem_array+=("$final_json_object")
 		
+		PrintMessage "Successfully parsed: Filesystem '$current_filesystem'" 0
+		
 		((filesystem_no++))
 	done < <(echo "$filesystem_utilization")
 	
 	final_filesystem_array=$(echo "${filesystem_array[@]}" | sed 's/,\([^,]*\)$/\1/')
 	
 	combined_json=$(echo "{\"filesystem_utilization\":[${final_filesystem_array[@]}]}" | jq '.')
-
-	echo "Converting filesystem utilization to JSON..."
+	
 	ConvertToJSON "$combined_json"
 }
 
@@ -73,14 +95,24 @@ function ConvertToJSON()
 	local value_to_convert=$1
 	
 	InsertToFile "$value_to_convert" $file_directory$file_name
-	echo -e "Successfully converted filesystem utilization to JSON!\n ► Filename: $file_name\n ► File directory: $file_directory"
+	
+	PrintMessage "Successfully converted filesystem utilization to JSON!\n ► Filename: $file_name\n ► File directory: $file_directory" 0
 }
 
 function Main()
 {
-	echo "Generating filesystem utilization..."
-	GenerateFileSystemUtilization
+	file_name_log="filesystem_util_to_json_${current_date}.log"
+	
+	{
+		PrintMessage "Generating filesystem utilization..." 0
+		GenerateFileSystemUtilization
+	} 2>&1 | tee -a "$file_name_log"
 }
 
-file_name_log="filesystem_util_to_json_${current_date}.log"
-Main 2>&1 | tee -a "$file_name_log"
+#==========================================================#
+# RUNTIME FUNCTIONS
+#==========================================================#
+
+Main
+
+#==========================================================#
